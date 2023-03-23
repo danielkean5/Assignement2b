9ae1573106bb35e07900ffdf8188cead4a8418f7
danielkean5 <124378867+danielkean5@users.noreply.github.com>
2023-03-23 14:16:10 +0000

    circles

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
+import math 
 
+diameter = input('Insert Diameter of a circle. \n')
+
+while True:
+  if diameter.isnumeric():
+    diameter = float(diameter)
+    area = math.pi*(diameter/2)**2
+    circumfrence = math.pi*diameter
+    print("The area of your circle is ",area)
+    print("The circumfrence of your cirlce is ",circumfrence)
+    break
+  else:
+    diameter = input('Unsuccesful. \n')
